0ca707edd9d78ccf8e850b1fc87b0d922b221d83
tBeauvisage <t.beauvisage@laposte.net>
2015-04-28 14:27:31 +0200

    phase1

--- Dahouet/src/com/thomas/dahouet/GUI/Navire.java
+++ Dahouet/src/com/thomas/dahouet/GUI/Navire.java
@@ -6,6 +6,8 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Image;
+import net.miginfocom.swing.MigLayout;
 
 public class Navire extends JFrame {
 
@@ -17,8 +19,11 @@ public class Navire extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new MigLayout("", "[424px]", "[251px]"));
+		
+		ImagePanel imagePanel = new ImagePanel((Image) null);
+		contentPane.add(imagePanel, "cell 0 0,grow");
 	}
 
 }
